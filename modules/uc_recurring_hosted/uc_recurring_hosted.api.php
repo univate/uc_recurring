306d221ad50b37b42f0a8cb1ec75ededee528d33
Chris Hood <chris@chris-laptop.(none)>
2010-01-14 11:24:57 +1000

    from cvs

---
+++ modules/uc_recurring_hosted/uc_recurring_hosted.api.php
@@ -0,0 +1,14 @@
+<?php
+// $Id: uc_recurring_hosted.api.php,v 1.1.2.1 2009/12/19 04:16:49 univate Exp $
+
+function hook_uc_recurring_hosted_subscription_alter(&$object) {
+
+}
+
+function hook_uc_recurring_hosted_subscription_save($object) {
+
+}
+
+function hook_uc_recurring_hosted_subscription_delete($id, $type = 'rfid') {
+
+}
