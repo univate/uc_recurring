4a604546e5b8d444affaf719d1764e09d17bb910
chris <chris@netbook.(none)>
2010-05-01 17:46:58 +1000

    add extra views field and filter handlers as well as hooks for allowing other modules to define other access methods

---
+++ modules/uc_recurring_access/uc_recurring_access.api.php
@@ -0,0 +1,21 @@
+<?php
+// $Id$
+
+/**
+ * @file
+ * Defines the hooks available in this module.
+ */
+
+/**
+ * Act on recurring access event.
+ */
+function hook_uc_recurring_access_apply($fee, $product) {
+
+}
+
+/**
+ * Allows other access types.
+ */
+function hook_uc_recurring_access_types_alter(&$access) {
+
+}
